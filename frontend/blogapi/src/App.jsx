f3cfc016a1b7b82570773e3c8bced79020149b50
wei <wmnv222666@163.com>
2024-03-21 11:58:30 -0400

    add frontend

---
+++ frontend/blogapi/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+
+function App() {
+
+
+
+
+  return (
+    <Box className="App">
+      <Typography variant="h1">Latest Posts</Typography>
+    </Box>
+  );
+}
+
+export default App;
+
