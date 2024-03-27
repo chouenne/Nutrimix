e2a8fe415b26dacbb4a0cf46fe2d3ab1278f1ba1
chouenne <134743005+chouenne@users.noreply.github.com>
2024-03-27 15:07:27 -0400

    initialize the backend from wei version

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -16,10 +16,6 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Configure DEFAULT_AUTO_FIELD
-
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
