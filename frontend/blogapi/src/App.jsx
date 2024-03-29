d9080d37575b806c1c890efa5bae01851498b442
wei <wmnv222666@163.com>
2024-03-29 12:29:13 -0400

    login and register

--- frontend/blogapi/src/App.jsx
+++ frontend/blogapi/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import RecipeDetail from './pages/RecipeDetail'
+import Register from './components/HomePageComponents/register'
+import Login from './components/HomePageComponents//login'
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/recipe/:recipeId" element={<RecipeDetail />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   </BrowserRouter>
--- frontend/blogapi/src/components/HomePageComponents/Header.jsx
+++ frontend/blogapi/src/components/HomePageComponents/Header.jsx
@@ -1,35 +1,27 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-// import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import MoreIcon from '@mui/icons-material/MoreVert';
+import InputBase from '@mui/material/InputBase';
 import logo from '../../assets/images/receipe-logo.svg';
+import axiosInstance from './axios';
+import Button from '@mui/material/Button';
+import { NavLink } from 'react-router-dom';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
-  borderRadius: '50px', 
+  borderRadius: '50px',
   backgroundColor: alpha(theme.palette.common.white, 0.15),
   '&:hover': {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginRight: theme.spacing(2),
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
+  width: '40%',
 }));
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
@@ -46,184 +38,91 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: 'dark',
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create('width'),
     width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
   },
 }));
 
 export default function Header() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // user login status
+  const [user, setUser] = useState({}); // user info
 
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  useEffect(() => {
+    const checkAuthentication = () => {
+      // Check if access token exists in local storage
+      const accessToken = localStorage.getItem('access_token');
+      if (accessToken) {
+        setIsAuthenticated(true);
+        // If needed, you can also decode and set user info here
+        // Example: setUser(decodedUserInfo);
+      } else {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
 
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
+    // Check authentication on component mount
+    checkAuthentication();
+  }, []);
+
+  const handleLogout = () => {
+    // Clear access token from local storage and reset state
+    localStorage.removeItem('access_token');
+    setIsAuthenticated(false);
+    setUser({});
   };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-    </Menu>
-  );
-
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton size="large" aria-label="show 4 new mails" color="dark">
-          <Badge badgeContent={4} color="error">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      {/* <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="show 17 new notifications"
-          color="inherit"
-        >
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem> */}
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="dark"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" style={{ backgroundColor: '#DFE0DE', boxShadow: 'none' }}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
+          <IconButton size="large" edge="start" color="inherit" aria-label="open drawer" sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
-					{/* <Typography variant="h6" component="div"> */}
-            <img src={logo} alt="Logo"style={{ width: 40, marginRight: 16 }} />
-						{/* </Typography> */}
-          
+          <img src={logo} alt="Logo" style={{ width: 40, marginRight: 16 }} />
           <Search>
             <SearchIconWrapper>
               <SearchIcon />
             </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
+            <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
           </Search>
           <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton size="large" aria-label="show 4 new mails" color="dark">
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            {/* <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-            >
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton> */}
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="dark"
-            >
-              <AccountCircle />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="dark"
-            >
-              <MoreIcon />
-            </IconButton>
-          </Box>
+          <div className='flex uppercase font-semibold'>
+            {isAuthenticated ? (
+              <div className='flex items-center gap-4'>
+                <div className='text-sm flex gap-2'>Hello <p className='underline text-midnight cursor-pointer'>@{user.username}</p></div>
+                <Button onClick={handleLogout} variant="contained" sx={{ bgcolor: '#f06292', color: 'white', textTransform: 'capitalize' }}>Logout</Button>
+              </div>
+            ) : (
+              <>
+                {/* <Link to='/login'><Button variant="contained" sx={{ bgcolor: '#f06292', color: 'white', textTransform: 'capitalize' }}>Login</Button></Link>
+                <Link to='/register'><Button variant="contained" sx={{ bgcolor: '#f06292', color: 'white', textTransform: 'capitalize' }}>Register</Button></Link> */}
+                  <Button
+                    href="#"
+                    color="primary"
+                    variant="outlined"
+                    sx={{ margin: '0 8px' }}
+                    component={NavLink}
+                    to="/login"
+                  >
+                    Login
+                  </Button>
+                  <Button
+                    href="#"
+                    color="primary"
+                    variant="outlined"
+                    sx={{ margin: '0 8px' }}
+                    component={NavLink}
+                    to="/register"
+                  >
+                    Register
+                  </Button>
+                </>
+            )}
+          </div>
         </Toolbar>
       </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
     </Box>
   );
 }
