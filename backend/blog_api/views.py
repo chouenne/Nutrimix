ed2b862c24bd0d13ab59bd63940d45ed09e38bbb
wei <wmnv222666@163.com>
2024-04-02 10:55:38 -0400

    for the category

--- backend/blog_api/views.py
+++ backend/blog_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from blog.models import Post
-from .serializers import PostSerializer
+from blog.models import Post, Category
+from .serializers import PostSerializer, CategorySerializer
 from rest_framework.permissions import (
     SAFE_METHODS,
     IsAuthenticated,
@@ -10,7 +10,8 @@ from rest_framework.permissions import (
     IsAdminUser,
     DjangoModelPermissions,
 )
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class PostUserWritePermission(BasePermission):
     message = "Editing posts is restricted to the author only."
@@ -40,6 +41,13 @@ class PostList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+@api_view(["GET"])
+def category(request):
+    categories = Category.objects.all()
+    serialize = CategorySerializer(categories, many=True)
+    return Response(serialize.data)
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
