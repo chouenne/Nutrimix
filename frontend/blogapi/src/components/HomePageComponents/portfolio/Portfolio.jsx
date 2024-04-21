207137c101bb563fedc7c5f9d2cf7a7ae6dbaca3
wei <wmnv222666@163.com>
2024-04-20 22:03:02 -0400

    modify style for the portfoio page

--- frontend/blogapi/src/components/HomePageComponents/portfolio/Portfolio.jsx
+++ frontend/blogapi/src/components/HomePageComponents/portfolio/Portfolio.jsx
@@ -61,7 +61,7 @@ const Portfolio = () => {
   return (
     <div className="portfolio" ref={ref}>
       <div className="progress">
-        <h1>Recipe </h1>
+        <h1>Standardize</h1>
         <motion.div style={{ scaleX }} className="progressBar"></motion.div>
       </div>
       {items.map((item) => (
